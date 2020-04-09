7f2adf8632b10f925f4c6c24bb48367cc9ad7b3a
Mark <mzakharo@ucsc.edu>
2020-04-08 17:54:33 -0700

    added Lab_1

---
+++ Lab_1/Lab_1.srcs/sources_1/new/LFSR_struct.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/08/2020 05:53:56 PM
+// Design Name: 
+// Module Name: LFSR_struct
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module LFSR_struct(
+    input clk,
+    input res_n,
+    input [7:0] data_in,
+    output [7:0] data_out
+    );
+endmodule
