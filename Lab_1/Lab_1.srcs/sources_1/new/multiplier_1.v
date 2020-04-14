fc47110922fe4aec8d4917734a1006b67618ea49
Mark <mzakharo@ucsc.edu>
2020-04-13 21:30:03 -0700

    cleaned up mult_1

--- Lab_1/Lab_1.srcs/sources_1/new/multiplier_1.v
+++ Lab_1/Lab_1.srcs/sources_1/new/multiplier_1.v
@@ -29,26 +29,25 @@ module multiplier_1(
     input [15:0] arg2,
     output reg [31:0] product
     );
-    integer i;
+    integer i;  //doubles as counter and enable for the accumulator
     always @(posedge start)begin
         done = 1'b0;
-        i = arg1 + 1;
+        i = arg1;
     end
-    always @(posedge clk)begin
-        i = i - 1;
-        if(i>0)begin
+    always @(posedge clk)begin  //values are updated each clock cycle if i > 0
+        if(i>1)begin
+            product = product + arg2;   //basic operation, adds arg2 (arg1) times
+            i = i - 1;   
+        end
+        else if(i==1)begin
             product = product + arg2;
-            
-        end
-
-        else if(i==0)begin
-            done = 1'b1;
+            i = i - 1;  //final decrement of i, disabling the functions of this black
+            done = 1'b1; //sets done high knowing this is the final accumulator operation
         end
     end
-    always @(negedge res_n)begin
+    always @(negedge res_n)begin    //resets all output signals and i to disable accumulation
         i = 1'b0;
         done = 1'b0;
         product = 1'b0;
     end
-    
 endmodule
