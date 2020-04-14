751397b2f8b985439f6f93a60b6a2807a5d4fbd5
hsliao <hsliao@ucsc.edu>
2020-04-14 16:56:08 -0700

    multiplier w/ state machine

--- Lab_1/Lab_1.srcs/sources_1/new/multiplier_1.v
+++ Lab_1/Lab_1.srcs/sources_1/new/multiplier_1.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/08/2020 06:56:39 PM
-// Design Name: 
-// Module Name: multiplier_1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module multiplier_1(
     input clk,
@@ -29,25 +9,45 @@ module multiplier_1(
     input [15:0] arg2,
     output reg [31:0] product
     );
-    integer i;  //doubles as counter and enable for the accumulator
-    always @(posedge start)begin
-        done = 1'b0;
-        i = arg1;
-    end
-    always @(posedge clk)begin  //values are updated each clock cycle if i > 0
-        if(i>1)begin
-            product = product + arg2;   //basic operation, adds arg2 (arg1) times
-            i = i - 1;   
-        end
-        else if(i==1)begin
-            product = product + arg2;
-            i = i - 1;  //final decrement of i, disabling the functions of this black
-            done = 1'b1; //sets done high knowing this is the final accumulator operation
-        end
-    end
-    always @(negedge res_n)begin    //resets all output signals and i to disable accumulation
-        i = 1'b0;
-        done = 1'b0;
-        product = 1'b0;
-    end
+    
+    reg [1:0] state;
+
+    integer count;
+        
+    always @(posedge clk) begin
+        case (state)
+            2'b00:
+                if (start) begin
+                    product = 0;
+                    count = 0;
+                    done = 1'b0;
+                    state = 2'b01;
+                end
+            2'b01:
+                if (count == arg2) begin
+
+                    done = 1'b1;
+                    state = 2'b10;
+                end
+                else begin
+                    product = product + arg1;
+                    count = count + 1;
+                    state = 2'b01;
+                end
+            2'b10:
+                if (!res_n) begin
+                    state = 2'b00;
+                end
+                else begin 
+                    done = 1'b1;
+                end
+            2'b11: state = 2'b00;
+            default: state = 2'b00;
+        endcase
+        
+    end 
+
+    
+    
 endmodule
+
