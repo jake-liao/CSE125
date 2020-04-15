3d598cf62f4190c03b565848d8cf3dd1f2d53a95
Mark <mzakharo@ucsc.edu>
2020-04-14 17:40:00 -0700

    mult 4

--- Lab_1/Lab_1.srcs/constrs_1/new/constraints.xdc
+++ Lab_1/Lab_1.srcs/constrs_1/new/constraints.xdc
@@ -1,8 +1,14 @@
 create_clock -period 10.000 -name clk -waveform {0.000 5.000} [get_ports clk]
-set_input_delay -clock [get_clocks clk] -min -add_delay 2.000 [get_ports {data_in[*]}]
-set_input_delay -clock [get_clocks clk] -max -add_delay 2.000 [get_ports {data_in[*]}]
+
 set_input_delay -clock [get_clocks clk] -min -add_delay 2.000 [get_ports res_n]
 set_input_delay -clock [get_clocks clk] -max -add_delay 2.000 [get_ports res_n]
-set_output_delay -clock [get_clocks clk] -min -add_delay 0.000 [get_ports {data_out[*]}]
-set_output_delay -clock [get_clocks clk] -max -add_delay 2.000 [get_ports {data_out[*]}]
 
+
+
+
+set_input_delay -clock [get_clocks clk] -min -add_delay 2.000 [get_ports {arg2[*]}]
+set_input_delay -clock [get_clocks clk] -max -add_delay 2.000 [get_ports {arg2[*]}]
+set_output_delay -clock [get_clocks clk] -min -add_delay 0.000 [get_ports {product[*]}]
+set_output_delay -clock [get_clocks clk] -max -add_delay 2.000 [get_ports {product[*]}]
+set_output_delay -clock [get_clocks clk] -min -add_delay 0.000 [get_ports done]
+set_output_delay -clock [get_clocks clk] -max -add_delay 2.000 [get_ports done]
--- Lab_1/Lab_1.srcs/sources_1/new/multiplier_4.v
+++ Lab_1/Lab_1.srcs/sources_1/new/multiplier_4.v
@@ -21,6 +21,56 @@
 
 
 module multiplier_4(
-
+    input clk,
+    input res_n,
+    input start,
+    output reg done,
+    input [15:0] arg1,
+    input [15:0] arg2,
+    output reg [31:0] product
     );
+    
+    reg [1:0] state;
+
+    integer count;
+        
+    always @(posedge clk) begin
+        case (state)
+            2'b00:
+                if (start) begin
+                    product = 0;
+                    count = 0;
+                    done = 1'b0;
+                    state = 2'b01;
+                end
+            2'b01:
+                if (count == 0) begin
+
+                    done = 1'b1;
+                    state = 2'b10;
+                end
+                else if(count >= 4)begin
+                    product = product + arg1*4;
+                    count = count - 4;
+                    
+                end
+                else begin
+                    product = product + arg1*count;
+                    count = 0;
+                    state = 2'b01;
+                    done = 1'b1;
+                end
+            2'b10:
+                if (!res_n) begin
+                    state = 2'b00;
+                end
+                else begin 
+                    done = 1'b1;
+                end
+            2'b11: state = 2'b00;
+            default: state = 2'b00;
+        endcase
+        
+    end 
+
 endmodule
